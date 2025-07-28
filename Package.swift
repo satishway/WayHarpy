94254fcc9e4cf930faee591d890518ca0934401c
Satish-Rajpurohit <>
2025-07-28 16:12:19 +0530

    Package updated

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WayHarpy",
+    defaultLocalization: "en",
     products: [
         .library(name: "WayHarpy", targets: ["WayHarpy"])
     ],
