8e66a05b4dced89d2348059d3fda87a97ba7c15a
Satish-Rajpurohit <>
2025-07-28 10:13:25 +0530

    SPM package update

--- Package.swift
+++ Package.swift
@@ -7,28 +7,14 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        .library(
-            name: "WayHarpy",
-            targets: ["WayHarpy"]
-        )
+        .library(name: "WayHarpy", targets: ["WayHarpy"])
     ],
     targets: [
         .target(
             name: "WayHarpy",
-            path: "Harpy",                    // <-- Location of .h, .m, and Harpy.bundle
-            publicHeadersPath: ".",          // <-- .h files are in Harpy/
-            resources: [
-                .process("Harpy.bundle")     // <-- Includes all localization .lproj folders
-            ]
-        ),
-        .target(
-            name: "WayHarpyAssets",          // Separate target for image assets
-            path: "Assets",
-            resources: [
-                .process("picForcedUpdate.png"),
-                .process("picOptionalUpdate.png"),
-                .process("picSkippedUpdate.png")
-            ]
+            path: "Harpy",
+            publicHeadersPath: ".",
+            resources: [.process("Harpy.bundle")]
         )
     ]
-) 
+)
